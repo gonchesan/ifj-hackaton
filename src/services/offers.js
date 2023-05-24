f67a9d711286272ff8c7543477c26abd473d69c5
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2023-05-24 18:07:57 -0300

    Fix the services and server config

--- src/services/offers.js
+++ src/services/offers.js
@@ -1,5 +1,5 @@
 export async function getAllOffers(query) {
-  const resp = await fetch(`/api/api/9/offer?q=${query}`, {
+  const resp = await fetch(`https://api.infojobs.net/api/9/offer?q=${query}`, {
     headers: {
       Authorization: `Basic MDliMTk0MjE4YmFkNGEyYzg1MDU4YjRkNGJiMTkwMTQ6RENUa05lamRRL3plRXBPN1VjQit0cXZlczBsbDRsMjZHZkFSYnd0RVY1M2JidkMralg=`,
     },
@@ -9,7 +9,7 @@ export async function getAllOffers(query) {
 }
 
 export async function getOfferById(id) {
-  const resp = await fetch(`/api/api/9/offer/${id}`, {
+  const resp = await fetch(`https://api.infojobs.net/api/9/offer/${id}`, {
     headers: {
       Authorization: `Basic MDliMTk0MjE4YmFkNGEyYzg1MDU4YjRkNGJiMTkwMTQ6RENUa05lamRRL3plRXBPN1VjQit0cXZlczBsbDRsMjZHZkFSYnd0RVY1M2JidkMralg=`,
     },
